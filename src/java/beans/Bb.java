324fbefe095a810ab7820b57ccf2f04cdaf691bb
hcs_20164040 <hcs_20164040@DESKTOP-D6SS0K3>
2018-12-12 15:01:36 +0900

    version4

--- src/java/beans/Bb.java
+++ src/java/beans/Bb.java
@@ -1,5 +1,10 @@
 package beans;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Serializable;
 import java.util.List;
 import javax.enterprise.context.SessionScoped;
@@ -14,6 +19,46 @@ import util.ItemUtil;
 @Named
 @SessionScoped
 public class Bb implements Serializable {
+    
+    private String title;
+    private String num;
+    private String place;
+    private String time;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+    
+    
+    
     private List<Item> item;
     
     {	
@@ -40,5 +85,33 @@ public class Bb implements Serializable {
         this.item = item;
     }
     
+    public String add(){
+        try {
+            
+            
+            
+            FileWriter file = new FileWriter("C:\\Users\\hcs_20164040\\Documents\\NetBeansProjects\\r3a218\\web\\resources\\data\\Item.csv", true);
+            
+            PrintWriter p = new PrintWriter(new BufferedWriter(file),"UTF-8");
+            
+            //ファイルに書き込む
+            p.println();
+            p.print(title+","+num+","+place+","+time);
+            
+            System.out.print("エラー１");
+            //ファイルを閉じる
+            p.close();
+            System.out.print("エラー2");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();    
+        System.out.print("エラー5");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.print("エラー3");
+        }
+        System.out.print("エラー4");
+        item = ItemUtil.getList("/resources/data/Item.csv");
+        return null;
+    }
     
 }
